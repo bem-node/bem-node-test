16242281d320a9a71e3758c34d74d3b89a5abe34
wtfil <filmuzhik@gmail.com>
2013-03-17 10:05:33 +0200

    Initail commit

---
+++ .bem/make.js
@@ -0,0 +1,27 @@
+var cwd = process.cwd();
+
+MAKE.decl('Arch', {
+
+    getBundlesLevels: function() {
+        return [
+            cwd + '/pages'
+        ];
+    }
+
+});
+
+MAKE.decl('BundleNode', {
+
+    getTechs: function() {
+
+        return [
+            'bemdecl.js',
+            'deps.js',
+            'css',
+            'js',
+            'priv.js',
+            'server.js'
+        ];
+    }
+
+});
---
+++ pages/index/index.bemdecl.js
@@ -0,0 +1,9 @@
+exports.blocks = [
+
+    // bem-node part
+
+    {name: 'i-router', mods: {init: 'auto'}},
+    {name: 'i-yccjs'}
+
+    // user part
+]
